53a116a0a25c18706cca509d5b7d701f9432d9f2
Akashamba <akashambashankar@gmail.com>
2020-12-31 09:25:30 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import {Switch, Route} from 'react-router-dom';
 import Navbar from './components/navbar/navbar.component.jsx';
 import Homepage from './pages/homepage/homepage.component.jsx';
 import Feedback from './pages/feedback/feedback.component.jsx';
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Switch>
-            <Route exact path='/' component={Homepage} />
-            <Route exact path='/:website' children={Feedback} />
-        </Switch>
+        <br/>
+        <div className="card">
+            <Switch>
+                <Route exact path='/' component={Homepage} />
+                <Route exact path='/:website' component={Feedback} />
+            </Switch>
+        </div>
       
     </div>
   );
--- src/pages/feedback/feedback.component.jsx
+++ src/pages/feedback/feedback.component.jsx
@@ -1,8 +1,108 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Input, Button, Form, Rate } from 'antd';
+import extractName from '../../utilities/website-name.js';
+import './feedback.styles.css'
 
 export default function Feedback(props) {
-    // console.log("props", props)
+
+    const [loading, setLoading] = useState(false);
+
+    const onFinish = (values) => {
+        setLoading(true);
+        console.log(values);
+        setTimeout(() => setLoading(false), 5000);
+    }
+
+    const onFinishFailed = (err) => {
+        console.log(err)
+    }
+
     return (
-        <div>{props.match.params.website.split("-").join(" ")}</div>
+        <div>
+            <br/>
+            <h2>{extractName(props.match.params.website)}</h2>
+            <br/>
+
+            <Form layout="vertical" onFinish={onFinish} onFinishFailed={onFinishFailed} >
+
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please enter your name',
+                    },
+                    ]}
+                >
+                    <Input size="large" placeholder="Name" />
+                </Form.Item>
+
+                <br/><br/>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please enter your Email Address',
+                    },
+                    ]}
+                >
+                    <Input size="large" type="email" placeholder="Email" />
+                </Form.Item>
+
+                <br/><br/>
+
+                <Form.Item
+                    label="How did you find this website?"
+                    name="find"
+                >
+                    <Input size="large" placeholder="How did you find this website?" />
+                </Form.Item>
+
+                <br/><br/>
+
+                <Form.Item
+                    label="How would you rate the website on a scale of 5?"
+                    name="rating"
+                >
+                    <div className="rate-container" ><Rate/></div>
+                </Form.Item>
+
+            <br/><br/>
+
+                <Form.Item
+                    label="What did you like about the website?"
+                    name="good"
+                >
+                    <Input.TextArea size="large" placeholder="What did you like about the website?" />
+                </Form.Item>
+
+                <br/><br/>
+
+                <Form.Item
+                    label="What can be improved?"
+                    name="improvement"
+                >
+                    <Input.TextArea size="large" placeholder="What can be improved?" />
+                </Form.Item>
+
+                <br/><br/>
+
+                <Form.Item
+                    label="Anything else?"
+                    name="other"
+                >
+                    <Input.TextArea size="large" placeholder="Anything else?" />
+                </Form.Item>
+
+                <br/><br/>
+
+                
+                <Button type="primary" loading={loading} htmlType="submit">Submit</Button>
+            </Form>
+        </div>
     )
 }
