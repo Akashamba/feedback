66e076dcb2548d305f63e6b8e0cc912036b311df
Akashamba <akashambashankar@gmail.com>
2020-12-31 12:32:14 +0530

    added thank you contents

--- src/pages/thank-you/thank-you.component.jsx
+++ src/pages/thank-you/thank-you.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function ThankYouPage() {
-    return(<div>Thank you</div>)
+    return(
+        <div>
+            <h1>Your response has been saved!</h1>
+            <p>Thank you for taking the time to review my website.</p>
+            <p>Your feedback is much appreciated.</p>
+            <p><b>- Akash Ambashankar</b></p>
+        </div>)
 }
